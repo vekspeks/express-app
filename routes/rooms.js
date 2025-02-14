6457312710ae40ddc69ad4a8af7a44f5a2191136
Vedran <vedransatvar@gmail.com>
2025-02-14 12:12:07 +0100

    .

--- routes/rooms.js
+++ routes/rooms.js
@@ -5,16 +5,16 @@ var db = require("../services/db");
 const roomsShema = require("../schemas/rooms");
 const { valid } = require('joi');
 
-router.get("/create", function(req, res, next){
+router.get("/create", function (req, res, next) {
     res.render("rooms/create")
 
 });
 
-router.post("/create", async function(req, res, next){
+router.post("/create", async function (req, res, next) {
     const result = roomsShema.validate(req.body);
 
-    if(result.error){
-        res.render("rooms/create", {error_validation: true})
+    if (result.error) {
+        res.render("rooms/create", { error_validation: true })
         return;
     }
 
@@ -24,10 +24,38 @@ router.post("/create", async function(req, res, next){
         const query = "INSERT INTO rooms (name) VALUES (?);"
         const stmt = await conn.prepare(query);
         const result = await stmt.execute([req.body.name]);
-        res.render("rooms/create", {success: true});
+        res.render("rooms/create", { success: true });
     } catch (error) {
-        res.render("rooms/create", {error_database: true});
-    } finally{
+        res.render("rooms/create", { error_database: true });
+    } finally {
+        conn.release();
+    }
+});
+
+router.get("/usage/:id", async function (req, res, next) {
+    const roomId = req.params.id;
+
+    let conn, room;
+    try {
+        conn = await db.getConnection();
+        const query = "SELECT * FROM rooms WHERE id = ?;";
+        const stmt = await conn.prepare(query);
+        const result = await stmt.execute([roomId]);
+        if (result.length === 1) {
+            room = result[0];
+        }
+        else {
+            res.render("rooms/usage", { invalid_id: true });
+        }
+        const query2 = "SELECT g.*, r.name FROM `usage` g, users r " +
+            "WHERE g.room_id = ? AND r.id = g.user_id ORDER BY g.signed_in DESC;";
+        const stmt2 = await conn.prepare(query2);
+        const result2 = await stmt2.execute([roomId]);
+        res.render("rooms/usage", { items: result2, room_id: roomId });
+    } catch (error) { 
+        console.log(error);
+
+    } finally {
         conn.release();
     }
 });
